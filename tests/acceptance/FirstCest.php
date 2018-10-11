eafa06b25037a3a00c82a2af603cbe82f6912c59
Marvin Calderón <2327239740101@ingenieria.usac.edu.gt>
2018-10-10 23:20:31 -0600

    Implementacion de pruebas funcionales para registro

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -2,18 +2,5 @@
 
 class FirstCest
 {
-    public function _before(AcceptanceTester $I)
-    {
-    }
-
-    // tests
-    public function tryToTest(AcceptanceTester $I)
-    {
-    }
-
-    public function funcionaPaginaPrincipal(AcceptanceTester $I) {
-        $I->amOnPage('/');
-        $I->see('La mejor tienda de laptops de la región.');
-    }
-
+    
 }
---
+++ tests/functional/RegistroCest.php
@@ -0,0 +1,71 @@
+<?php
+
+class RegistroCest
+{
+    // public function funcionamientoRegistroUsuario(FunctionalTester $I) {
+    //
+    //     $I->amOnPage('/');
+    //     $I->click('Registro');
+    //     $I->wait(10);
+    //     $I->fillField('email', 'pruebafuncional3@gmail.com');
+    //     $I->fillField('password', '123456');
+    //     $I->click(['id'=>'btnAction']);
+    //     $I->amGoingTo("check whether any JS alert appears and accept it");
+    //     $I->executeInSelenium(function (\Facebook\WebDriver\WebDriver $webdriver) use ($I) {
+    //         $webdriver->wait(1.5, 200)->until(
+    //             WebDriverExpectedCondition::alertIsPresent()
+    //         );
+    //         if($webdriver->switchTo()->alert()->getText() == "Usuario registrado correctamente.") {
+    //             $webdriver->switchTo()->alert()->accept();
+    //         }else{
+    //             $I->fail("Fallo");
+    //         }
+    //     });
+    //
+    // }
+    //
+    // public function funcionamientoUsuarioRepetido(FunctionalTester $I) {
+    //
+    //     $I->amOnPage('/');
+    //     $I->click('Registro');
+    //     $I->wait(10);
+    //     $I->fillField('email', 'pruebafuncional3@gmail.com');
+    //     $I->fillField('password', '123456');
+    //     $I->click(['id'=>'btnAction']);
+    //     $I->amGoingTo("check whether any JS alert appears and accept it");
+    //     $I->executeInSelenium(function (\Facebook\WebDriver\WebDriver $webdriver) use ($I) {
+    //         $webdriver->wait(1.5, 200)->until(
+    //             WebDriverExpectedCondition::alertIsPresent()
+    //         );
+    //         if($webdriver->switchTo()->alert()->getText() == "El usuario proporcionado ya se encuentra registrado.") {
+    //             $webdriver->switchTo()->alert()->accept();
+    //         }else{
+    //             $I->fail("Fallo");
+    //         }
+    //     });
+    //
+    // }
+    //
+    // public function funcionamientoRegistroCamposVacios(FunctionalTester $I) {
+    //
+    //     $I->amOnPage('/');
+    //     $I->click('Registro');
+    //     $I->wait(10);
+    //     $I->fillField('email', '');
+    //     $I->fillField('password', '');
+    //     $I->click(['id'=>'btnAction']);
+    //     $I->amGoingTo("check whether any JS alert appears and accept it");
+    //     $I->executeInSelenium(function (\Facebook\WebDriver\WebDriver $webdriver) use ($I) {
+    //         $webdriver->wait(1.5, 200)->until(
+    //             WebDriverExpectedCondition::alertIsPresent()
+    //         );
+    //         if($webdriver->switchTo()->alert()->getText() == "Ocurrió un error al obtener los datos para registrarlos.  Intente de nuevo más tarde.") {
+    //             $webdriver->switchTo()->alert()->accept();
+    //         }else{
+    //             $I->fail("Fallo");
+    //         }
+    //     });
+    //
+    // }
+
+}
